c2644c9421c52f62dd8803b40d83850b5b5ad8c3
florianingerl <imelflorianingerl@gmail.com>
2022-10-20 07:44:54 +0200

    Adds quizes for maths in English and French!

---
+++ assets/js/quizlesmaths.js
@@ -0,0 +1,21 @@
+let questionslesmaths = [
+	{
+		question: "Qu'est-ce la racine carré de neuf?",
+		imageUrl: "assets/img/lesmaths/racinecarredeneuf.png",
+		type: "multiple choice",
+		options: ["quatre virgule cinq, c'est à dire 4,5", "trois"],
+		correct: 1
+
+	},
+
+	{
+		question: "Qu'est-ce la troisième carré de huit?",
+		imageUrl: "assets/img/lesmaths/troisiemecarredehuit.png",
+		type: "multiple choice",
+		options: ["deux", "trois huitième", "huit tiers"],
+		correct: 0
+	}
+
+];
+
+new Quiz("quizlesmaths", questionslesmaths);
