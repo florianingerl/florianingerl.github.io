1a7f83840a1bc54969878b3c4dd16e93b74bdf76
florianingerl <imelflorianingerl@gmail.com>
2022-10-20 07:44:54 +0200

    Adds quizes for maths in English and French!

---
+++ assets/js/quizmathenanglais.js
@@ -0,0 +1,21 @@
+let questionsmathenanglais = [
+	{
+		question: "What is the square root of nine?",
+		imageUrl: "assets/img/lesmaths/racinecarredeneuf.png",
+		type: "multiple choice",
+		options: ["four point five, that means 4.5", "three"],
+		correct: 1
+
+	},
+
+	{
+		question: "What is the third root of eight?",
+		imageUrl: "assets/img/lesmaths/troisiemecarredehuit.png",
+		type: "multiple choice",
+		options: ["two", "three over eight", "eight over three"],
+		correct: 0
+	}
+
+];
+
+new Quiz("quizmathenanglais", questionsmathenanglais);
