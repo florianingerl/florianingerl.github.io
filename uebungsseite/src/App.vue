aa26d210b501325c5f40ee41c12f12d47965ad32
florianingerl <imelflorianingerl@gmail.com>
2023-03-30 19:11:04 +0200

    Adds Übungsseite!

---
+++ uebungsseite/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <RegexExercise />
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import RegexExercise from './components/RegexExercise.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld, RegexExercise
+  }
+
+  /*computed: {
+    exercises(){
+      fetch("./public/regexexercises.json")
+      .then((response) => response.json())
+      .then((data) => { console.log(data); return data; } );
+
+    }
+  }*/
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
