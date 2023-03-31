de0deef94eac4ac65ba69cbf3db84599e4206844
florianingerl <imelflorianingerl@gmail.com>
2023-03-31 10:42:35 +0200

    Writes two good Regex Exercises

--- uebungsseite/src/components/RegexExercise.vue
+++ uebungsseite/src/components/RegexExercise.vue
@@ -1,6 +1,8 @@
 <template>
+<div class="container">
+<h2>Exercise {{ this.exercise.id }}</h2>
     <p>
-        Please replace all i=i+5; by i+=5;!
+        {{ this.exercise.instruction }}
     </p>
 
     <p>Find:</p>
@@ -9,35 +11,58 @@
     <input type="text" name="replace" id="replace" v-model="replaceby"/>
     <br>
     <button @click="replaceAll">Replace all</button>
+    <span class="correct" v-if="score==1">
+      You have solved this exercise correctly!
+    </span>
+    <div v-if="score==-1">
+      <span class="incorrect" >
+        You haven't replaced correctly! Click reset to try again!
+      </span>
+      <button @click="reset">Reset</button>
+    </div>
     <br>
     <textarea  v-model="editor">
         
     </textarea>
    
-   
+   </div>
 </template>
 
 <script>
 export default {
   name: 'RegexExercise',
+  props: ["exercise"],
  
   data() {
     return {
         find : "",
         replaceby : "",
-        editor: "let i =7;\ni=i+5;\ni=i*78;\nlet s = 5;\ns = s - 1;\ns = s/2;\n"
-        /*
-            Regex: (j|s)\s*=\s*(\1)\s*(\+|\*)\s*(\d+)
-            ReplaceString: $1$3=$4
-        */
+        editor: this.exercise.editor,
+        score: 0
     }
   },
 
   methods: {
     replaceAll(){
         console.log("The button was clicked!");
+        let editorText = this.editor;
         let regEx = new RegExp(this.find, 'g')
-        this.editor = this.editor.replace(regEx, this.replaceby);
+        this.editor = editorText.replace(regEx, this.replaceby);
+
+        let regExCor = new RegExp(this.exercise.find, 'g');
+        let editorTextCor = editorText.replace(regExCor, this.exercise.replaceby);
+
+        if(this.editor == editorTextCor ){
+            this.score = 1;
+        }
+        else {
+            this.score = -1;
+        }
+    },
+
+    reset(){
+      this.editor = this.exercise.editor;
+      this.score = 0;
     }
   }
 }
@@ -45,8 +70,23 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.container {
+  padding: 5px;
+  margin: 5px;
+  border-radius: 5px;
+  border: 1px solid green;
+}
+
 textarea {
     width: 500px;
-    height: 500px;
+    height: 50px;
+}
+
+.correct {
+  color: green;
+}
+
+.incorrect {
+  color: red;
 }
 </style>
