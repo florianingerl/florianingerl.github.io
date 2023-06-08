b439a156804857dc209332961590bcbcd1f22743
florianingerl <imelflorianingerl@gmail.com>
2023-06-08 05:26:05 +0200

    Updates Stornobedingungen!

---
+++ assets/js/test.js
@@ -0,0 +1,9 @@
+console.log("Hello Florian!");
+
+console.log( "Hello" instanceof String );
+
+console.log( 3 instanceof Number );
+
+if( 3 instanceof Number){
+    console.log("3 is an instance of number");
+}
