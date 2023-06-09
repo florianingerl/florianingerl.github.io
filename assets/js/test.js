ca2dda9340ba9d4875fb36adad9099e7ead1e36e
florianingerl <imelflorianingerl@gmail.com>
2023-06-09 09:07:20 +0200

    Deletes unused file!

--- assets/js/test.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello Florian!");
-
-console.log( "Hello" instanceof String );
-
-console.log( 3 instanceof Number );
-
-if( 3 instanceof Number){
-    console.log("3 is an instance of number");
-}
