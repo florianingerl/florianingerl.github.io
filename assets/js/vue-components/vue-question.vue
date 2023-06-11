490bb83e6cecd833de84d18fb9178452403fe0be
florianingerl <imelflorianingerl@gmail.com>
2023-06-11 07:03:10 +0200

    Changes nutrition quiz so that is uses Vue.js!

---
+++ assets/js/vue-components/vue-question.vue
@@ -0,0 +1,93 @@
+<template>
+<div class="d-flex flex-row">
+<img :src="question.imageUrl" />
+<div>
+<p>
+{{ question.question }}
+</p>
+<div>
+<div v-for="option in question.options" :key="option" :class="{correct : validated && option.checked && option.correct}" :class="{notcorrect : validated && option.checked && !option.correct}">
+<input type="checkbox" v-model="option.checked"/>
+<label>{{option.option}}</label>
+</div>
+
+
+</div>
+
+<div class="d-flex flex-row">
+<button @click="validate">Aufgabe überprüfen</button>
+<button @click="showSolution">Lösung anzeigen</button>
+<button v-if="hasNextButton">Nächste Aufgabe</button>
+</div>
+
+</div>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: "VueQuestion",
+  setup(){
+     console.log("The setup function is executed!");
+     
+  },
+  props: ['question', 'hasNextButton'],
+  
+  data() {
+    return {
+      validated : false
+    };
+  },
+  watch: {
+    question(newQuestion, oldQuestion){
+      console.log("The watcher was called!");
+      this.validated = false;
+    }
+  },
+  methods: {
+    validate(){
+      console.log("The validate button was clicked!");
+      this.validated = true;
+    },
+    showSolution(){
+      this.validated = false;
+      this.question.options.forEach( option => { option.checked = option.correct; });
+    }
+  },
+  mounted() {
+    console.log('Vue Question Component mounted');
+
+    if(!this.question){
+      this.question = 
+      {
+		question: "Das ist eine Test-Frage aus vue-question.vue?",
+		imageUrl: "assets/img/rawfood/brokolisalat.JPG",
+		type: "multiple choice",
+		options: [{option:"Süßlupinen", correct: false },
+     {option:"Brokoli", correct: false }, 
+     {option:"Karotte", correct: false },
+      {option:"Löwenzahn", correct: true },
+       {option:"Weizen", correct: false }]
+	    };
+    }
+
+    this.question.options.forEach( option => { option.checked = false; });
+  }
+};
+</script>
+
+<style scoped>
+
+.correct {
+  background-color: #7bf27b
+}
+
+.notcorrect {
+  background-color: #fc5c5c
+}
+
+img {
+  height: 200px;
+}
+</style>
---
+++ assets/js/vue-components/vue-quiz.vue
@@ -0,0 +1,73 @@
+<template>
+    <vue-question v-if="i < questions.length" :question="questions[i]"></vue-question>
+    <div v-if="i == questions.length">
+        Gratulation! Du hast alle Fragen des Quiz beantwortet!
+    </div>
+    <button v-if="i < questions.length-1" @click="nextExerciseClicked">Nächste Aufgabe</button>
+</template>
+
+<script>
+
+export default {
+
+  components: {
+    
+    VueQuestion : Vue.defineAsyncComponent(() =>
+    loadModule('assets/js/vue-components/vue-question.vue', options)),
+  
+  },
+  props: ['questions'],
+
+  setup(){
+     console.log("The setup function is executed!");
+     
+  },
+  
+  data() {
+    return {
+       i : 0
+    };
+  },
+  methods: {
+     nextExerciseClicked(){
+        this.i++;
+        console.log(this.i);
+     }
+  },
+  mounted() {
+    console.log('Vue Questions Component mounted');
+
+    if(!this.questions){
+      this.questions = [
+             {
+		question: "Das ist eine Test-Frage aus vue-quiz.vue?",
+		imageUrl: "assets/img/rawfood/brokolisalat.JPG",
+		type: "multiple choice",
+		options: [{option:"Süßlupinen", correct: false },
+     {option:"Brokoli", correct: false }, 
+     {option:"Karotte", correct: false },
+      {option:"Löwenzahn", correct: true },
+       {option:"Weizen", correct: false }]
+	    },
+
+         {
+		question: "Das ist die zweite Test-Frage aus vue-quiz.vue!",
+		imageUrl: "assets/img/rawfood/brokolisalat.JPG",
+		type: "multiple choice",
+		options: [{option:"Süßlupinen", correct: false },
+     {option:"Brokoli", correct: false }, 
+     {option:"Karotte", correct: false },
+      {option:"Löwenzahn", correct: true },
+       {option:"Weizen", correct: false }]
+	    }
+
+       ];
+    }
+
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
