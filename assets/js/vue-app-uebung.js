bb4044920527a2d3bde514e1cd3c4531ba4f9598
florianingerl <imelflorianingerl@gmail.com>
2023-08-07 19:18:39 +0200

    Adds a Lückentext!

---
+++ assets/js/vue-app-uebung.js
@@ -0,0 +1,32 @@
+// js/vue-app.js
+
+const { createApp } = Vue;
+const { loadModule } = window['vue3-sfc-loader'];
+
+const options = {
+  moduleCache: {
+    vue: Vue,
+  },
+  getFile(url) {
+    return fetch(url).then((resp) =>
+      resp.ok ? resp.text() : Promise.reject(resp)
+    );
+  },
+  addStyle(styleStr) {
+    const style = document.createElement('style');
+    style.textContent = styleStr;
+    const ref = document.head.getElementsByTagName('style')[0] || null;
+    document.head.insertBefore(style, ref);
+  },
+  log(type, ...args) {
+    console.log(type, ...args);
+  },
+};
+
+const app = createApp({
+  components: {
+    VueGaps : Vue.defineAsyncComponent(() =>
+    loadModule('assets/js/vue-components/vue-gaps.vue', options))
+  
+  },
+}).mount('#gaps');
