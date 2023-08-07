e6b2041b529e03f9ec34a488ca4e034a42c6ea08
florianingerl <imelflorianingerl@gmail.com>
2023-08-07 19:18:39 +0200

    Adds a Lückentext!

---
+++ assets/js/vue-components/vue-gaps.vue
@@ -0,0 +1,39 @@
+<template>
+<h1>Remplis les trous!</h1>
+<div>
+<span v-for="gap in gaps" >
+  {{ gap.text }} <input type="text">
+</span>
+
+</div>
+    
+</template>
+
+<script>
+
+export default {
+
+  components: {
+  
+  },
+  props: ['gapfile'],
+
+  setup(){
+     console.log("The setup function is executed!");
+     
+  },
+  
+  data() {
+    return {
+       gaps : [{text: "Florian est le ", gap: "frère"}, {text: " de sa ", gap: "soeur"}, {text: " qui s'appelle ", gap: "Sonja"}]
+    };
+  },
+  methods: {
+     
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
