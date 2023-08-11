1c47716c8714b2114516deca1102832cd89bd710
florianingerl <imelflorianingerl@gmail.com>
2023-08-11 17:10:42 +0200

    Corrects the menu!

--- assets/js/vue-components/french/alimentation/vue-alimentation1.vue
+++ assets/js/vue-components/french/alimentation/vue-alimentation1.vue
@@ -1,4 +1,5 @@
 <template>
+<div v-if="$route.params.page == 1">
 <h1>Cours d'alimentation vivante 1</h1>
     Cette académie est le fruit d’expériences et de recherches personnelles. Son but
     est de vous transmettre des connaissances et mon vécu afin de vous aider à
@@ -40,6 +41,50 @@ et je vais faire attention à {ce que} mon propre corps me dit. {Ce qui} me donn
 je déciderai moi-même de suivre les conseils ou pas.{}" /> </VueExercise>
 
 Mettons-nous toutes et tous au service de la vie.
+</div>
+
+<div v-if="$route.params.page == 2">
+<h1>Cours d'alimentation vivante 2</h1>
+<h3>Qu'est-ce que l'Hygiénisme ?</h3>
+L'hygiénisme est l'art de vivre en bonne santé par le respect des lois de
+la nature et par la bonne connaissance des aliments spécifiques à
+l'espèce humaine.
+Cette conception est révolutionnaire parce qu'elle est aux antipodes des modes de
+pensée habituels.
+Contrairement à la médecine officielle qui traite les symptômes, l’Hygiénisme part
+du principe de supprimer la cause pour faire disparaitre l'effet.
+<VueExercise><VueQuestion :question=" {
+		question: 'Qu\'est-ce que l\'hygiénisme?',
+		type: 'multiple choice',
+		options: [{option:'L\'art de suivre les conseils du médecin', correct: false },
+     {option:'L\'art de suivre les lois de la nature', correct: true }, 
+     ]
+	    }" :hasNextButton="false" /></VueExercise>
+L’hygiénisme est donc un enseignement tiré directement de l’observation des Lois
+de la vie. En un mot, c'est un véritable art de vivre.
+L'Hygiénisme enseigne les principes de base pour tous, mais chacun doit doser
+chaque facteur de santé selon son propre pouvoir d'assimilation et devenir ainsi
+l'artisan de sa santé.
+<VueExercise instruction="Remplis les trous avec tout,toute,tous,toutes ou chaque!">
+<VueGaps gaptext="{Tout} ce que je viens de lire me semble logique. {Chaque} phrase fait du sens.
+Dans {toute} ma vie, je n'ai jamais entendu parler de {tout} cela. {Toute} ma santé depend aussi
+de mon alimentation.{}" /> </VueExercise>
+
+L’école hygiéniste est simple. Elle redonne au corps son plein potentiel de
+guérison. On parlera d’homéostasie*. On ne contre pas, on accompagne. On
+permet aux mécanismes de régénération de faire leur travail en agissant sur
+l’équilibre du terrain.
+<VueExercise instruction="Remplis les trous avec le gérondif des verbes répondre et lire!">
+<VueGaps gaptext="En {lisant} ce livre, je peux passer mon temps d'une manière utile. En {répondant} aux questions,
+j'arrive à ne pas tomber en sommeil.{}" /> </VueExercise>
+C’est pour cela qu’un minimum de connaissances en physiologie vous sera
+nécessaire.
+*La loi de l’homéostasie dit que : « Tout système laissé à lui-même revient
+spontanément à l’état d’équilibre en passant par des processus
+régulateurs »
+
+</div>
+
 </template>
 
 <script>
--- assets/js/vue-components/vue-nested-menu.vue
+++ assets/js/vue-components/vue-nested-menu.vue
@@ -2,14 +2,10 @@
 
 <div class="vertical-menu">
   <p>
-    <button v-if="selectedMenus.length > 0" @click="menuUpClicked">Up</button> <span v-for="menu in selectedMenus" @click="goBackToMenuClicked(menu)">{{menu}} &gt; </span>
+    <button v-if="selectedMenus.length > 0" @click="menuUpClicked">Up</button> <span v-for="menu in selectedMenus" @click="goBackToMenuClicked(menu)">{{menu.name}} &gt; </span>
   </p>
-  <!-- <a href="#" class="active">Home</a>
-  <a href="#">Link 1</a>
-  <a href="#">Link 2</a>
-  <a href="#">Link 3</a>
-  <a href="#">Link 4</a> -->
-  <a v-for="item in currentMenuItems" :class="{ active: item.name === selectedMenus[selectedMenus.length - 1] }" @click="menuClicked(item.name)">{{item.name}}</a>
+
+  <a v-for="item in currentMenuItems" :class="{ active: selectedMenus.length > 0 && item.name === selectedMenus[selectedMenus.length - 1].name }" @click="menuClicked(item)">{{item.name}}</a>
 
 </div>
 
@@ -27,10 +23,12 @@ export default {
   components: {
   
   },
-  props: ['gapfile'],
+
 
   mounted(){
-    console.log("Mounted function is executed!");
+    console.log("Mounted function of vue-nested-menu is executed!");
+    console.log(this.$router.options.routes);
+    this.items = this.$router.options.routes;
 
   },
   setup(){
@@ -40,41 +38,28 @@ export default {
   data() {
     return {
       selectedMenus : [],
-      items: [
-          { name: 'Französisch', path: '/french', children: [
-            { name: 'Konditionalsätze 1', path: '/phraseconditionnel1' },
-            { name: 'Konditionalsätze 2', path: '/phraseconditionnel2' },
-            { name: 'Alimentation', children: [
-              { name: 'Alimentation1', path: '/alimentation1' },
-              { name: 'Alimentation2', path: '/alimentation2' }
-            ] }
-          ]},
-          { name: 'Englisch', path: '/english', children: [
-            { name: 'Emphasis', path: '/emphasis', children: [
-              { name: 'Emphasis 1', path: '/emphasis1' },
-              { name: 'Emphasis 2', path: '/emphasis2' }
-            ]},
-           { name: 'RawFood', path: '/rawfood', children: [
-              { name: 'Raw Food 1', path: '/rawfood/1/' },
-              { name: 'Raw Food 2', path: '/rawfood/2/' }
-            ]},
-          ]},
-          { name: 'Github', path: 'https://github.com' },
-        ]
+      items: []
     };
   },
   methods: {
-     menuClicked(name){
+     menuClicked(menu){
+        console.log(menu);
         //If aktuelles Menu has no children, it must be removed!
         if( this.selectedMenus.length > 0 ){
-            let currentMenu = this.currentMenuItems.findIndex( e => e.name === this.selectedMenus[this.selectedMenus.length - 1]);
+            let currentMenu = this.currentMenuItems.findIndex( e => e.name === this.selectedMenus[this.selectedMenus.length - 1].name );
             if(currentMenu != -1 ){
                 this.selectedMenus.pop();
             }
         }
-        let cmi = this.currentMenuItems.find( e => e.name === name );
-        this.selectedMenus.push(name);
-        this.$router.push(cmi.path);
+        let cmi = this.currentMenuItems.find( e => e.name === menu.name );
+        this.selectedMenus.push(menu);
+        let path="";
+        this.selectedMenus.forEach( item => {
+          path = path + "/" + item.path;
+        });
+        path = path.replace("//","/");
+        path = path.replace(":page","1");
+        this.$router.push(path);
      },
      menuUpClicked(){
         this.selectedMenus.pop();
@@ -88,7 +73,7 @@ export default {
      currentMenuItems(){
         let cMI = this.items;
         for(let i=0; i < this.selectedMenus.length; ++i){
-            let c = cMI.find( e => e.name === this.selectedMenus[i] );
+            let c = cMI.find( e => e.name === this.selectedMenus[i].name );
             if( c.children ){
                 cMI = c.children;
             }
