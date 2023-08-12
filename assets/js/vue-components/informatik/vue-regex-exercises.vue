f6b758f5a7120bbae79fae79556e84055016a26b
florianingerl <imelflorianingerl@gmail.com>
2023-08-12 18:00:24 +0200

    Solved strange error with vue-regex-exercise.vue

---
+++ assets/js/vue-components/informatik/vue-regex-exercises.vue
@@ -0,0 +1,43 @@
+<template>
+  <RegexExercise v-for="exercise in exercises" :exercise="exercise" />
+</template>
+
+<script>
+import RegexExercise from './vue-regex-exercise.vue';
+
+export default {
+  name: 'RegexExercises',
+  components: {
+    RegexExercise
+  },
+  data(){
+    return {
+      exercises : []
+    }
+  },
+
+  mounted() {
+    console.log("Mounted!!!");
+
+    fetch("uebung/informatik/regexexercises.json")
+      .then((response) => response.json())
+      .then((data) => { console.log(data); this.exercises = data; console.log(this.exercises); console.log(this.exercises[0]); } );
+  
+    
+  }
+
+
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
