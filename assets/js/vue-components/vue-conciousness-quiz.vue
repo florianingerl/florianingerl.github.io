fe3f80d6be9c1141cbfb08ea330b0c0dd71d3198
florianingerl <imelflorianingerl@gmail.com>
2023-09-10 09:41:54 +0200

    Adds conciousness quiz but hasn't a function yet1

---
+++ assets/js/vue-components/vue-conciousness-quiz.vue
@@ -0,0 +1,49 @@
+<template>
+    <vue-quiz :lg="lg" :questions="questions"></vue-quiz>
+</template>
+
+<script>
+
+export default {
+
+  components: {
+    
+    VueQuiz : Vue.defineAsyncComponent(() =>
+    loadModule('assets/js/vue-components/vue-quiz.vue', options)),
+  
+  },
+
+  props: ['lg'],
+
+  setup(){
+     console.log("The setup function is executed!");
+     
+  },
+  
+  data() {
+    return {
+       questions: 
+      [
+	{
+		question: "In welcher Situation bist du bewusster?",
+		questionFr: "Dans quelle situation est-ce que tu es le plus conscient?",
+		imageUrl: "assets/img/cs/atem.jpg",
+		type: "multiple choice",
+		options: [ {option: "Wenn du gerade deinen eigenen Atem fühlen kannst.", correct: true }, { option: "Wenn du weißt, dass dein Gegenüber gerade unrecht hat.", correct: false } ],
+		optionsFr:  [ {option: "Quand tu peux sentir ta propre respiration", correct: true }, { option: "Quand tu sais que les autres ont tort", correct: false } ],
+		correct: 0
+
+	}
+
+	
+
+]
+    };
+  },
+ 
+};
+</script>
+
+<style scoped>
+
+</style>
