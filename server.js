a116c915afb388861430268a2967c2b91a70f0a4
florianingerl <imelflorianingerl@gmail.com>
2023-12-02 09:10:00 +0100

    Adds session managemet!

--- server.js
+++ server.js
@@ -1,11 +1,67 @@
-var express = require('express');
+const express = require('express');
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
+
 var app = express();
 
-app.use( express.static (".") );
+//Sessions in NodeJS
+//https://www.section.io/engineering-education/session-management-in-nodejs-using-expressjs-and-express-session/
+const oneDay = 1000 * 60 * 60 * 24;
+
+//session middleware
+app.use(sessions({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: false
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const myusername = 'user1'
+const mypassword = 'mypassword'
+
+// a variable to save a session
+var session;
+
+
+
 
 app.get('/amen', function (req, res) {
   res.send('Hello World!');
 });
+
+
+app.get('/',(req,res) => {
+  session=req.session;
+  if(session.userid){
+      res.send("Welcome User <a href=\'/logout'>click to logout</a>");
+  }else
+  res.sendFile('views/index.html');
+});
+
+app.post('/user',(req,res) => {
+  if(req.body.username == myusername && req.body.password == mypassword){
+      session=req.session;
+      session.userid=req.body.username;
+      console.log(req.session)
+      res.send(`Hey there, welcome <a href=\'/logout'>click to logout</a>`);
+  }
+  else{
+      res.send('Invalid username or password');
+  }
+});
+
+app.get('/logout',(req,res) => {
+  req.session.destroy();
+  res.redirect('/');
+});
+
+
+app.use( express.static (".") );
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
