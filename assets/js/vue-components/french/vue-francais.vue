87ea5473d99e2cb38c6e225ea88a04a21a96b180
florianingerl <imelflorianingerl@gmail.com>
2023-12-11 11:21:19 +0100

    Corrects errors!

---
+++ assets/js/vue-components/french/vue-francais.vue
@@ -0,0 +1,65 @@
+<template>
+<div>
+    <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>N°</th>
+        <th>Titre</th>
+        <th>Autheur</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td><router-link to="/french/grammaire/en">Pronom en</router-link></td>
+        <td>Flori10</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td><router-link to="/french/grammaire/dontouduquel">Dont ou duquel</router-link></td>
+        <td>Flori10</td>
+      </tr>
+    </tbody>
+  </table>
+
+</div>
+
+ 
+</template>
+
+<script>
+
+export default {
+  name: "VueFrancais",
+  components: {
+  
+  },
+  props: [],
+
+  data() {
+    return {
+       
+    };
+  },
+
+  mounted(){
+     
+
+
+  },
+  setup(){
+   console.log("The setup function is executed!");
+  },
+  
+  
+  methods: {
+    
+  }
+}
+</script>
+
+<style scoped>
+
+
+
+</style>
