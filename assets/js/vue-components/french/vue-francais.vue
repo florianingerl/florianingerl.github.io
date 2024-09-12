17f0ce3dd065a461799b3329d444f3ca176785a6
Florian Ingerl <imelflorianingerl@gmail.com>
2024-09-12 20:32:05 +0200

    The choosing of the dictionary works now much better!

--- assets/js/vue-components/french/vue-francais.vue
+++ assets/js/vue-components/french/vue-francais.vue
@@ -11,67 +11,67 @@
     <tbody> 
     <tr>
 <td>13</td>
-       <td><router-link to="/french/livres/livresurfred/1">Un livre sur Fred</router-link></td>
+       <td><router-link to="/fr/french/livres/livresurfred/1">Un livre sur Fred</router-link></td>
        <td>Flori10</td>
     </tr>
       <tr>
 <td>12</td>
-       <td><router-link to="/french/prononciation/sone">Le son é</router-link></td>
+       <td><router-link to="/fr/french/prononciation/sone">Le son é</router-link></td>
        <td>Flori10</td>
     </tr>
     <tr>
 <td>11</td>
-       <td><router-link to="/french/grammaire/neexpletif">Ne explétif</router-link></td>
+       <td><router-link to="/fr/french/grammaire/neexpletif">Ne explétif</router-link></td>
        <td>Flori10</td>
     </tr>
     <tr>
 <td>10</td>
-       <td><router-link to="/french/grammaire/celuioucelle">Celui,celle,ceux ou celles</router-link></td>
+       <td><router-link to="/fr/french/grammaire/celuioucelle">Celui,celle,ceux ou celles</router-link></td>
        <td>Flori10</td>
     </tr>
     <tr>
     <td>9</td>
-       <td><router-link to="/french/vocabulaire/encore">Le mot encore</router-link></td>
+       <td><router-link to="/fr/french/vocabulaire/encore">Le mot encore</router-link></td>
        <td>Flori10</td>
     </tr>
     <tr>
        <td>8</td>
-       <td><router-link to="/french/grammaire/dontouduquelcresson">Dont ou duquel - exerice avec des tamis de cresson</router-link></td>
+       <td><router-link to="/fr/french/grammaire/dontouduquelcresson">Dont ou duquel - exerice avec des tamis de cresson</router-link></td>
        <td>Flori10</td>
     </tr>
     <tr>
         <td>7</td>
-        <td><router-link to="/french/grammaire/rajouterouajouter">Rajouter ou ajouter</router-link></td>
+        <td><router-link to="/fr/french/grammaire/rajouterouajouter">Rajouter ou ajouter</router-link></td>
         <td>Flori10</td>
       </tr>
     <tr>
         <td>6</td>
-        <td><router-link to="/french/grammaire/lionpassesimpleouimparfait">L'histoire d'un lion - Passé simple ou l'imparfait</router-link></td>
+        <td><router-link to="/fr/french/grammaire/lionpassesimpleouimparfait">L'histoire d'un lion - Passé simple ou l'imparfait</router-link></td>
         <td>Flori10</td>
       </tr>
      <tr>
         <td>5</td>
-        <td><router-link to="/french/grammaire/imparfaitouplusqueparfait">Imparfait ou Plus-que-parfait</router-link></td>
+        <td><router-link to="/fr/french/grammaire/imparfaitouplusqueparfait">Imparfait ou Plus-que-parfait</router-link></td>
         <td>Flori10</td>
       </tr>
     <tr>
         <td>4</td>
-        <td><router-link to="/french/grammaire/passesimple">imparfait ou passe simple</router-link></td>
+        <td><router-link to="/fr/french/grammaire/passesimple">imparfait ou passe simple</router-link></td>
         <td>Flori10</td>
       </tr>
       <tr>
         <td>3</td>
-        <td><router-link to="/french/grammaire/conditionelle3">Phrase de type conditionelle 3</router-link></td>
+        <td><router-link to="/fr/french/grammaire/conditionelle3">Phrase de type conditionelle 3</router-link></td>
         <td>Flori10</td>
       </tr>
       <tr>
         <td>2</td>
-        <td><router-link to="/french/grammaire/dontouduquel">Dont ou duquel</router-link></td>
+        <td><router-link to="/fr/french/grammaire/dontouduquel">Dont ou duquel</router-link></td>
         <td>Flori10</td>
       </tr>
       <tr>
         <td>1</td>
-        <td><router-link to="/french/grammaire/en">Pronom en</router-link></td>
+        <td><router-link to="/fr/french/grammaire/en">Pronom en</router-link></td>
         <td>Flori10</td>
       </tr>
       
---
+++ assets/js/vue-components/vue-dict.vue
@@ -0,0 +1,123 @@
+<template>
+<div @dblclick="openDict">
+
+    <h1>Double clique sur n'importe quel mot sur ce cite pour une traduction!</h1>
+    Select your preferred dictionary:
+     <select  v-model="selectedDict">
+  <option disabled value="">Please select one</option>
+   <option v-for="dict in dictionaries">{{ dict.name }}</option>
+</select>
+
+    <router-view/>
+</div>
+
+ 
+</template>
+
+<script>
+
+export default {
+  name: "VueDict",
+  components: {
+  
+  },
+  props: [],
+
+  data() {
+    return {
+       selectedDict : "The free dictionary French - French",
+       dictionaries : []
+    };
+  },
+
+  watch:{
+    $route (to, from){
+        this.setDictionaries();
+    }
+  } ,
+
+  mounted(){
+     
+   console.log("The mounted function of VueFr is executed!" );
+
+   this.setDictionaries();
+
+  },
+  setup(){
+   console.log("The setup function of VueFr is executed!");
+     },
+  
+  
+  methods: {
+    setDictionaries() {
+         console.log(this.$route.params.language );
+   if( this.$route.params.language === 'french' ){
+    this.dictionaries = [ { name: "Word Reference French - German", url: "https://www.wordreference.com/frde/" },
+                         { name: "Word Reference French - English", url: "https://www.wordreference.com/fren/" }, 
+                         { name: "Larousse French - French", url: "https://www.larousse.fr/dictionnaires/francais/"},
+                         { name: "The free dictionary French - French", url: "https://fr.thefreedictionary.com/" }];
+   }
+   else if( this.$route.params.language === 'english' ){
+     this.dictionaries = [ { name: "Word Reference English - German", url: "https://www.wordreference.com/ende/" },
+                         { name: "Word Reference English - French", url: "https://www.wordreference.com/enfr/" }, 
+                         { name: "Oxford Learner's dictionary English - English", url: "https://www.oxfordlearnersdictionaries.com/definition/english/"},
+                         { name: "The free dictionary English - English", url: "https://en.thefreedictionary.com/" }];
+   }
+
+   this.selectedDict = this.dictionaries[0].name;
+
+    },
+    openDict(event){
+        console.log("A method in my vue component is executed!");
+        console.log(this.$stupidProp);
+        console.log(this.$window);
+        console.log(this.window);
+
+       console.log(this.$router.$stupidProp );
+       console.log(this.$router.stupidProp );
+       console.log(this.$router.$window2);
+       console.log(this.$router.window2);
+
+
+        console.log("event.target = " + event.target );
+        let s = this.window.getSelection();
+        console.log("s = " + s );
+          var range = s.getRangeAt(0);
+          var node = s.anchorNode;
+          
+        
+          // Find starting point
+          while(range.toString().search(/[^\w]/) != 0) {                 
+             range.setStart(node,(range.startOffset -1));
+          }
+          range.setStart(node, range.startOffset +1);
+          
+          // Find ending point
+          do{
+            range.setEnd(node,range.endOffset + 1);
+    
+         }while(range.toString().search(/[^\w]/) == -1 && range.toString().trim() != '');
+         
+         // Alert result
+         var str = range.toString().trim();
+         console.log(str);
+          var baseUrl = "https://fr.thefreedictionary.com/";
+         let i = this.dictionaries.findIndex( (dict) => dict.name === this.selectedDict );
+         if( i!= -1 ){
+            baseUrl = this.dictionaries[i].url;
+         }
+         window.open(baseUrl + str, "_blank").focus();
+       
+      
+       
+        console.log("This script has been executed!");  
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+
+
+</style>
