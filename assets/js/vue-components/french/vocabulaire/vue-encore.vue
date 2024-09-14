f7aafe1b778c435d9e222466bb2a53da158c4c5d
Florian Ingerl <imelflorianingerl@gmail.com>
2024-09-14 17:11:56 +0200

    MCGaps works for normal gaps and mc gaps!

--- assets/js/vue-components/french/vocabulaire/vue-encore.vue
+++ assets/js/vue-components/french/vocabulaire/vue-encore.vue
@@ -47,20 +47,20 @@ export default {
   
   data() {
     return {
-       gaptext: [ "J'ai terminé mon travail, je vais en demander encore un peu ! {De surcroît, en outre}",
-"Erwin n'a pas encore fini ses devoirs. {Opposition à déjà}",
-"Ne me pose pas encore la même question ! {Fait à répétition}",
-"L'enfant n'a pas encore brossé ses dents. {Opposition à déjà}",
-"J'ai encore le temps de jouer avec toi si tu veux ! {Contraire de ne... plus.}",
-"Mentir et s'en réjouir est encore pire que de justement mentir .{ degré plus grand}",
-"Pour ce poste, il te faut une longue expérience et encore faut-il que tu réussisses tous les entretiens préliminaires. {En addition, un autre}",
-"Max n'a pas encore visité Londres. Il s'y rendra bientôt. {Opposition à déjà}",
-"Encore une fois des spaghetti! Maman, tu ne peux jamais faire quelque chose d'autre? {l'impatience devant le renouvellement ou la continuation de quelque chose}",
-"Vous êtes encore en retard ! Je ne peux plus supporter ce comportement. {Fait à répétition}",
-"Il ne suffit pas d'avoir un don pour le football, encore faut-il de l'endurance et beaucoup d'entraînement. {En addition, un/une autre}",
-"Pourrais-je avoir encore une tasse de thé ? {En addition, un/une autre}",
-"Les pousses de blé sont encore mieux pour la santé que les bouillies de céréales fraîches de blé . {degré plus grand}",
-"Encore toi qui a crée un nouveau test sur francaisfacile ! {l'impatience devant le renouvellement ou la continuation de quelque chose}"
+       gaptext: [ "J'ai terminé mon travail, je vais en demander encore un peu ! {De surcroît, en outre|}",
+"Erwin n'a pas encore fini ses devoirs. {Opposition à déjà|}",
+"Ne me pose pas encore la même question ! {Fait à répétition|}",
+"L'enfant n'a pas encore brossé ses dents. {Opposition à déjà|}",
+"J'ai encore le temps de jouer avec toi si tu veux ! {Contraire de ne... plus.|}",
+"Mentir et s'en réjouir est encore pire que de justement mentir .{ degré plus grand|}",
+"Pour ce poste, il te faut une longue expérience et encore faut-il que tu réussisses tous les entretiens préliminaires. {En addition, un autre|}",
+"Max n'a pas encore visité Londres. Il s'y rendra bientôt. {Opposition à déjà|}",
+"Encore une fois des spaghetti! Maman, tu ne peux jamais faire quelque chose d'autre? {l'impatience devant le renouvellement ou la continuation de quelque chose|}",
+"Vous êtes encore en retard ! Je ne peux plus supporter ce comportement. {Fait à répétition|}",
+"Il ne suffit pas d'avoir un don pour le football, encore faut-il de l'endurance et beaucoup d'entraînement. {En addition, un/une autre|}",
+"Pourrais-je avoir encore une tasse de thé ? {En addition, un/une autre|}",
+"Les pousses de blé sont encore mieux pour la santé que les bouillies de céréales fraîches de blé . {degré plus grand|}",
+"Encore toi qui a crée un nouveau test sur francaisfacile ! {l'impatience devant le renouvellement ou la continuation de quelque chose|}"
        ]
     };
   }
