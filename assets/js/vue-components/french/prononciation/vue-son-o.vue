c03a3e1e3a89fc235530144badee79739587bbca
Florian Ingerl <imelflorianingerl@gmail.com>
2024-09-19 21:49:14 +0200

    Corrects a few errors!

--- assets/js/vue-components/french/prononciation/vue-son-o.vue
+++ assets/js/vue-components/french/prononciation/vue-son-o.vue
@@ -55,8 +55,8 @@ export default {
 " sur laquelle il y avait pas mal de gens dans leur bat{eau}. Ils chantaient des chans{o}ns et les enfants jouaient avec" +
 " des ballons. Utiliser le m{o}t balle au lieu de ball{o}n serait f{aux} si sa taille n'est pas si grand. Pas sûr si cela v{au}t la peine" +
 " maintenant de vous expliquer la différence entre bille, balle et ballon ou cela ne vous intéresse pas. Lorsque j'ai rencontré" +
-" une feu rouge, j'ai toujours vérifier qu'il n'y avait pas de flics alentour avant de la traverser. Parce que je n'aime pas" +
-" les flics sur mes tal{o}ns. Au contraire, je suis un garç{o}n qui chérit sa liberté. Mais j'ai vu la p{o}lice qui avait arreté" +
+" une feu rouge, j'ai toujours vérifié qu'il n'y avait pas de flics alentour avant de la traverser. Parce que je n'aime pas" +
+" les flics sur mes tal{o}ns. Au contraire, je suis un garç{o}n qui chérit sa liberté. Mais j'ai vu la p{o}lice qui avait arrêté" +
 " un homme qui avait tr{o}p chargé son chari{ot}. Quand je suis rentré, je n'ai pas mangé de gât{eau} mais des pousses de br{o}k{o}li. " +
 " Ça v{au}t toujours la peine d'en faire bien que ce soit b{eau}coup de boul{ot} de l'arr{o}ser. Maintenant, c'est la fin de" +
 " mon histoire. A bient{ô}t !"
