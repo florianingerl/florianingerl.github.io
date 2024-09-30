e28aad45bf1eb60f213456c7afc4a5466c6279f5
Florian Ingerl <imelflorianingerl@gmail.com>
2024-10-01 01:18:19 +0200

    Finishes the story with the cat detective!

--- assets/js/vue-components/english/shortstories/vue-anna-cat.vue
+++ assets/js/vue-components/english/shortstories/vue-anna-cat.vue
@@ -18,6 +18,7 @@ But his phone was in the bag so he stayed close by the bag just in case he had a
 The cat, whose name was Gareth by the way, would be given instructions by his manager on how to proceed with the following difficult matter:
 Unhappily, Gareth accepted these intructions because he needed to keep his job and earn a living to be able to support his nine small kittens and his beautiful cat wife. However, the matter was really delicate and difficult so he had to work a lot.
 Time to explain this in a bit more detail. The cat worked as a detective.
+
 <VueExercise lg="en" instruction="You think that our cat detective should bring these things on his missions. Form sentences with in case by linking each item with its justification">
 <p>Items: a razor, an umbrella, a cell phone, <span style="{text-decoration: line-through }">a watch</span>, a map</p>
 <p>Justification: it may rain, <span style="{text-decoration: line-through}">the cat might want to know the exact time</span>, the cat might get lost, the cat might want to get rid of its long hair, the might might get bored and want to play a game</p>
@@ -37,6 +38,7 @@ A female customer had consulted their detective company, because she had no trus
 Genuinely speaking, hard work!
 Despite his strict manager and the hard work, Gareth loved his job and he was committed to survey this cat to his best abilities. So when his manager assigned him the mission, he said:
  "I will do my best, sir!"
+
 <VueExercise lg="en" instruction="Fill in the gaps with any,no, anybody, nobody, anything or nothing!">
 <VueMCGaps lg="en" :same="true" :gaptext="['The cat doesn\'t want to eat {any|} fish today.',
 '{No|} fish is allowed in her diet because fish make her fat.',
@@ -50,16 +52,19 @@ Despite his strict manager and the hard work, Gareth loved his job and he was co
 Gareth had already followed the husband of that jealous wife until the restroom.
 That the husband had stayed there for 2 hours was indeed suspicious. A business like that doesn't take that long! So further inquiries were necessary.
 Gareth stayed nearby the restroom door and while watching the door, he started to come up with ideas for this strange behaviour. First, he thought that this cat had an affaire but during the two hours, there was no female cat going in or out. So he had to think more...
+
 <VueExercise lg="en" instruction="Fill in the gaps with the given verb in either the past progressive or the simple past!">
 <VueMCGaps lg="en" :gaptext="['While the cat {was sitting} (sit) on a loo in the restroom, her cell phone suddenly {rang} (ring).',
 'While the cat {was talking} (talk) to her boss, somebody turned (turn) the lights of the restroom off.',
 'While the cat {was trying} (try) to find her way out of the restroom in the dark, a neighbouring cubicle suddenly {exploded}(explode).']">
 </VueMCGaps>
 </VueExercise>
+
 Suddenly, a deafening sound came out of the cabin. Was this the sound of a non-well-digested piece of meat?
 Gareth wasn't sure but he had no choice than to stay and keep waiting. When his client's husband finally showed up from the cabin, he smiled at Gareth, not knowing that Gareth was there to spy on him.
 "What have you done in that cabin for 2 hours?" Gareth wanted to know. Although detectives usually don't act like that, Gareth chose fo this direct option because he wanted to finish the job quickly and spend time with his family.
 "I think my wife packed me some expired food for lunch. You know, we don't really have a good marriage. Sometimes I think she wants to poison me. But luckily, I'm feeling alright now, answered the suspicious cat.
+
 <VueExercise lg="en" instruction="Fill in the gaps with an adjective out of suspicious, precious, ostentatious and superstitious!">
 <VueMCGaps lg="en" :same="true" :gaptext="['The cat wears a very {precious|}, golden watch on her left arm.',
 'The {superstitious|} cat always gets up at exactly 4:44 a.m. because it thinks that this will bring her luck.',
@@ -72,141 +77,34 @@ Gareth wasn't sure but he had no choice than to stay and keep waiting. When his 
 Not being sure whether Gareth should believe him or not, he just nodded his head as an answer.
 "If I knew what else your wife does wrong, I would be able to help you by talking to her. You probably don't know, that your wife consulted the detective company I work for and paid a lot of money in order to have you tracked down."
 Gareth knew that he had just broken his promise to stay in the shadows as a detective by declaring who he was to this cat, but suddenly he had a strange feeling that maybe it was the wife who was doing something bad.
+
+
+<VueExercise lg="en" instruction="Fill in the gaps with the correct phrasal verb with up or down!">
+<VueMCGaps lg="en" :gaptext="['You still have to pay for the meeting although you didn\'t {show|hide|appear} up.',
+'The cat detective {laid|brought|said} down all the evidence in front of the judge.',
+'The accused husband could not {bring|take|go} up any defences in front of the judge.']">
+
+</VueMCGaps>
+</VueExercise>
+
 "I will try to sum up what my wife does wrong: First of all, she never shuts up and always talks although I desperately need silence. Secondly, she gets up too early and wakes me up too while doing it. And last but not least, she never finishes a project but always gives up once a small obstacle gets in her way. Observing that demotivates me."
 "Hm, what I thought before was completely wrong. I believed that you were up to something illegal. But now I just think you need couple therapy with your wife to save your marriage."
 "Luckily, I have already had 5 women and that's why I am an expert in these matters and exactly the right person to help you. Most important thing to understand: Talking or explaining more to your wife is of no use. Concerning the poisoned food your wife gives you: If eating it is not an option, just burry it somewhere and thank your wife for it.
+<VueExercise lg="en" instruction="Put the words in the correct order to form sentences!">
+<VueWordOrder lg="en" :sentences="['I have never had a relationship.',
+'I haven\'t had a relationship yet.',
+'I can always occupy myself with English books.'
+]">
+
+</VueWordOrder>
+</VueExercise>
+
 "Thank you for your great advice, I will keep it in mind. However, I'm not sure this will be enough to fix our relationship. Actually, I'm planning to escape in secret to Africa next week. Please don't tell her."
 But that's a really great idea! I have always wanted a holiday in Africa. Your wife will pay my company so I can track you down until Africa and I will be able to give you more advice there. If you want to visit other places after Africa, your wife will probably also pay for it.
 "That's a wonderful idea because I hate to travel alone so I'm happy if you can come with me in this way."
 In the end, this mission turned into an amazing travel experience for Gareth and he was even able to form a friendship with this cat. He started to like his job and didn't even mind his manager any longer.
 
-
-
-
-Natürlich verstaut eine Katze ihre Sachen nicht in einer Aktentasche,
-obwohl sich in der Tasche genug Platz finden würde, für alles was eine Katze so zum Leben braucht.
-Vielleicht bewachte die Katze die Aktentasche nur für ihr Herrchen, aber eigentlich wäre das die Aufgabe eines Wachhundes.
-"Ob ich dem Fischer einen Fisch abluchsen könnte? Ich gebe ihm mein bestes und herzzerreißendes Miauen, dann klappt es bestimmt.", sinnierte die Katze.
-Angekommen neben des Fischers Standes auf dem Marktplatz, fing die Katze, die übrigens 
-Melli hieß, ganz süß zu miauen an. Dabei schielte sie zu den Fischen, um ihren Wunsch einer Fischmahlzeit damit auszudrücken.
-<VueExercise lg="de" instruction="Bilde Sätze mit um...zu+Infinitiv, ohne...zu+Infinitiv oder anstatt...zu+Infinitiv wie in den Beispielen!">
-<p>Beispiel 1:</p>
-<p>Er, Hunger, stillen, etwas, essen</p>
-<p>Er isst etwas, um seinen Hunger zu stillen.</p>
-
-<p>Beispiel 2:</p>
-<p>Fernsehen schauen, Hausaufgaben, machen, Er </p>
-<p>Er schaut Fernsehen, anstatt seine Hausaufgaben zu machen.</p>
-
-<p>Beispiel 3:</p>
-<p>Er, gehen, auf Widersehen sagen</p>
-<p>Er geht, ohne auf Widersehen zu sagen.</p>
-
-<ol>
-<li>Gustav, Chef, an jemandem vorbei gehen, jdn grüßen</li>
-<li>Camille, Urlaub machen, arbeiten, lieber</li>
-<li>Katze, putzen, Fell, schmutzig, sein</li>
-</ol>
-<VueHideShow lg="de">
-<ol>
-<li>Gustav geht an seinem Chef vorbei, ohne ihn zu grüßen.</li>
-<li>Camille macht lieber Urlaub, anstatt zu arbeiten.</li>
-<li>Die Katze putzt ihr Fell, um nicht schmutzig zu sein.</li>
-</ol>
-</VueHideShow>
-</VueExercise>
-
-
-"Wenn ich dir einen kleinen Fisch gebe, begann der Fischer, dann möchte ich, dass du ihn woanders verspeist und nicht noch mehr hungrige Mäuler anziehst!" Melli bestätigte die Aussage mit einem Miauen und einem freudigen Wedeln ihres Schwanzes.
-Jedoch ist natürlich eine Aktentasche nicht geeignet, um einen kleinen Fisch darin zu transportieren, sondern in einer Aktentasche transportiert man Akten.
-<VueExercise lg="de" instruction="Fülle die Lücken mit sondern, aber, denn, und oder oder" >
-<VueMCGaps lg="de" :gaptext="['Die Katze heißt nicht Melissa, {sondern} sie heißt Melli.',
-'Die Katze kann zwar nicht bellen, {aber} sie kann miauen.',
-'Die Katze isst einen Fisch, {denn} sie hat Hunger.',
-'Du bezahlst den Fisch {oder} ich gebe ihn dir nicht.',
-'Die Katze mag gerne Fisch {und} Mäuse.']">
-
-</VueMCGaps>
-</VueExercise>
-
-Melli schob die Aktentasche mit ihrem Näschen zum Fischer und sagte ihm so, er solle die kleine Makrele darin verstauen. "Das geht doch nicht!", rief er lachend und setzte nach mit, "Deinem Herrchen würde das sicher nicht gefallen, wenn seine Aktentasche nach Fisch riechen würde!"
-
-<VueExercise lg="de" instruction="Bilde Konditionalsätze wie in den Beispielen!">
-
-<p>Beispiel 1:</p>
-<p>Katze, sehen, Maus, essen (Konditionalsatz der Vergangenheit)</p>
-<p>Wenn die Katze eine Maus gesehen hätte, hätte sie die Maus gegessen.</p>
-
-<ol>
-<li>Katze, Geld, haben, Fisch, bezahlen (Konditionalsatz der Vergangenheit)</li>
-<li>Katze, keinen Hunger, kein Fisch, wollen (Konditionalsatz der Vergangenheit)</li>
-<li>Katze, angeln, können, Fische fangen (Konditionalsatz mit irrealer Bedingung) </li>
-</ol>
-
-<VueHideShow lg="de">
-<ol>
-<li>Wenn die Katze Geld gehabt hätte, hätte sie den Fisch bezahlt.</li>
-<li>Wenn die Katze keinen Hunger gehabt hätte, hätte sie keinen Fisch gewollt.</li>
-<li>Wenn die Katze angeln könnte, würde sie sich selbst Fische fangen.</li>
-</ol>
-</VueHideShow>
-</VueExercise>
-
-
-Bei Fisch in einer Aktentasche war der Handel gestoppt wie bei einer roten Ampel. Beide überlegten, wie die Ampel wieder grün schalten könnten und die Katze machte zuerst einen Vorschlag:
-Die Katze, die ihren kleinen Fisch unter keinen Umständen verlieren wollte, öffnete plötzlich ihr Maul und gab dem sich anbahnenden Gähnen nach.
-"Na also, da haben wir ja schon eine Lösung.", kam es nun vom Fischer, welcher den Fisch der Katze flugs ins Maul steckte.
-Ungünstigerweise wurde die Katze Melli beim Transport des Fisches in ihrem Maul von einer Gruppe von streunenden Katzen beobachtet. Diese streunenden Katzen waren sehr ungebildet und hingen den ganzen Tag lang nur faul herum und fragten sich jetzt, wo Melli den leckeren Fisch herhatte.
-
-<VueExercise lg="de" instruction="Finde das Gegenteil">
-<VueMCGaps lg="de" :gaptext="['öffnen -> {schließen}', 'stoppen -> {starten}', 'klein -> {groß}', 'verlieren -> {gewinnen}', 'faul -> {fleißig}']">
-
-</VueMCGaps>
-
-</VueExercise>
-
-Als Melli darauf nicht wirklich antworten wollte, um dem Fischer keine Probleme zu bereiten, richtete sich die Meute von Katzen gegen sie. Diese nun fauchenden Ungetüme versuchten der armen Melli den Weg zu versperren und sie in eine Ecke der Gasse zu drängen.
-Wie schade! Hätte Melli jetzt das Nudelholz ihrer Großmutter zur Hand, so hätte sie sich damit gut zur Wehr setzten können. Ihre Großmutter hatte öfter Nudeln mit Fisch gekocht.
-In dem kurzen Moment jedoch, wo Melli in alten Erinnerungen schwelgte, war sie den Fisch losgeworden.
-Die Katze, deren Magen knurrte, wollte sich jedoch noch nicht geschlagen geben. Irgendwo musste es doch etwas, für sie zum Fressen geben.
-
-<VueExercise lg="de" instruction="Mache einen Satz aus den zwei Sätzen indem du einen Relativsätz mit Genitiv wie im Beispiel benutzt!">
-<p>Beispiel 1:</p>
-<p>Die Katze lief davon und bezahlte nicht. In ihrem Maul war der Fisch.</p>
-<p>Die Katze, in derem Maul der Fisch war, lief davon und bezahlte nicht.
-
-<ol>
-<li>Das Fell der Katze war dreckig. Die Katze ging unter die Dusche.</li>
-<li>Die Zähne der Katze waren scharf. Die Katze zerriss die Aktentasche aus Versehen.</li>
-<li>Die Stimme des Fischers war sehr kräftig. Der Fischer machte lautstark Werbung für seine Fische.</li>
-</ol>
-
-<VueHideShow lg="de">
-<li>Die Katze, deren Fell dreckig war, ging unter die Dusche.</li>
-<li>Die Katze, deren Zähne scharf waren, zerriss die Aktentasche aus Versehen.</li>
-<li>Der Fischer, dessen Stimme sehr kräftig war, machte lautstark Werbung für seine Fische.</li>
-</VueHideShow>
-</VueExercise>
-
-Keinen Blick in die Schaufenster verschwendend und von ihrem knurrenden Magen getrieben, lief die Katze zurück zum Fischer, wo sie wieder zum Miauen begann.
-Der Fischer war diesmal jedoch nicht allein. An seinem Verkaufsstand, war ein Kunde verkleidet als Osterhase, der seine Bestellungen abholte. Ob der Mann im Kostüm heute Waschtag und deswegen nichts anderes zum Anziehen hatte? Den seltsamen Mann ignorierend maunzte sie sich die Seele aus dem Leib.
-Der Fischer war jetzt völlig verwirrt. Hier der Lärm der miauenden Katze, die doch eben gerade einen Fisch bekommen hatte und jetzt wieder da war, und dort dieser Mann im Osterhasenköstum, obwohl gar kein Ostern war.
-
-<VueExercise lg="de" instruction="Fülle die Lücken mit dem gegebenen Verb im Präteritum aus!">
-<VueMCGaps lg="de" :gaptext="['Die Katze {lief} (laufen) nach Hause.', 'Der Fischer {rief} (rufen) nach der Polizei', 'Die streunenden Katzen {gingen} (gehen) leer aus.', 'Die Katze {hatte} (haben) Hunger.']">
-
-</VueMCGaps>
-</VueExercise>
-
-"Hier ihr bestellter Fisch Hr. Kimmer und ich hoffe sie sind wie immer zufrieden!"
-"Natürlich, ihr Fisch ist schließlich der beste in der Gegend!"
-Beide wünschten sich noch einen schönen Tag und beendeten ihr Gespräch im Eiltempo.
-Der Fischer war eigentlich ein versierter Geschäftsmann. Die Katze hatte zwar nur 50 Cent in ihrer Aktentasche und sein Fisch kostete 1 Euro, dennoch ließ er sich auf den vergünstigten Handel ein, um dem verkaufsschädlichen Katzenjammer an seinem Stand
-ein Ende zu bereiten.
-Überglücklich ob des geglückten Deals, nahm die Katze den gekauften Fisch, doch als sie ihn fressen wollte rief jemand ihren Namen, "Melli! Wo treibst du dich schon wieder rum, meine Mama wird mir den Hosenboden versohlen, wenn wir nicht pünktlich zu Hause sind!" Der kleine Malte nahm die Katze samt Aktentasche auf seine Arme und lief geschwind mit seinem Haustier nach Hause, wo beide hoffentlich keine Standpauke zu hören bekamen.
-
-
-THE END
+<h2>The end</h2>
 
 </div>
 
@@ -220,6 +118,7 @@ import VueQuestion from "./../../vue-question.vue";
 import VueExercise from "./../../vue-exercise.vue";
 import VueWholeSentence from "./../../vue-whole-sentence.vue";
 import VueHideShow from "./../../vue-hideshow.vue";
+import VueWordOrder from "./../../vue-word-order.vue";
 import VueImage from "./../../vue-image.vue";
 
 export default {
@@ -231,7 +130,8 @@ export default {
      VueWholeSentence,
      VueHideShow,
      VueImage,
-     VueMCGaps
+     VueMCGaps,
+     VueWordOrder
   },
 
   mounted(){
