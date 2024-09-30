fb0f7b3055774b4fa977706a47c93105a8b1e89f
Florian Ingerl <imelflorianingerl@gmail.com>
2024-09-30 10:04:26 +0200

    Connaitre ou savoir à présent de l'indicatif

---
+++ assets/js/vue-components/english/shortstories/vue-anna-turtoise.vue
@@ -0,0 +1,212 @@
+<template>
+<div>
+
+<h1>Eine Katze, die Fisch mag</h1>
+<h4><i>von Florian Ingerl und Anna Scakacs</i></h4>
+
+<img style="float: right; height: 200px" src="assets/img/deutsch/katze.JPG"></img>
+
+<p>Once upon a time, there was a turtoise who was very happy most of the time.
+Her name was Clara and she used to cry a lot when she was a small tutoise but over time, she learnt to be more optimistic and have a positive view on life.
+Because of her positive attitude towards life, she attracted only fellow tortoises who were also very positive and creative.
+She could have stayed unhappy all her life if she hadn't changed her perspective. But thankfully she did and so she managed to meet these other amazing turtoises and they became a tight-knit (=close) group of friends.</p>
+
+<VueExercise>
+
+</VueExercise>
+<p>
+Anton was the funniest of the group and often told jokes about humans or snakes and there was also Bryan, who was eating all the time and weighted a lot.
+Anton and Clara knew that they shouldn't always invite Bryan to have nice dinners together because of his weight problem but they also really appreciated food and dinner-time was the best occasion for the three friends to share what happened to them during the day.
+Bryan had invited everyone for a dinner today and so Clara, Anton and Bryan were looking forward to that evening and expected Anton to tell funny stories.
+If the three friends knew how many other turtoises would show up, they wouldn't cook that much food. They expected the whole tutoise village to come to this dinner but in the end, unfortunately, only a few animals showed up for the dinner party.
+Fortunately, Anton was in a good mood and - after everyone had sat down and gotten their salads - began to tell an anecdote: "Yesterday, the idea came to my mind that - because the salads in the supermarkets are so expensive - they must be really high quality and I went in there to buy one."
+</p>
+
+"Yes, I also went to the supermarket yesterday and I saw the price and the quality of the salads, commented Clara. "But next time it might be better if we planted our own salads and had organic food for dinner."
+Anton shortly appreciated Clara's comment by smiling into her direction, then continued his story: "Not having any money in my pockets, I simply took the salad in my mouth to the cassier and asked him whether I can bring him the money later and take the salad now with me."
+Bryan being a bit surprised by Anton's anecdote, asked his friend: "And so did the cashier let you leave without paying?"
+"No!" answered Anton. "He wasn't happy that I had taken the salad already in my mouth and that I had no money. I suggested of course to simply put the salad back."
+"What a funny turtoise you are, Anton!" said Bryan laughing. "I hope they didn't ban you from entering this supermarket again because we will need more food for our dinner party next week, added the food-obssessed turtoise.
+"I am listening to you but I don't understand you, because your mouth is still halfful, Bryan." - said Clara. "Please finish eating first before you speak!"
+"In case I was rude, I apologize for my poor manners, Clara. You are right, I should first finish eating my food but I like to eat a lot and also talk a lot so I sometimes do both at the same time, said Bryan.
+"Can I continue with my story?" asked Anton who was a bit annoyed about having been interrupted. In reply to this, the other two stopped talking and looked attentively in the direction of Anton to encourage him to continue.
+Without waiting another second, Anton happily continued his story: "So no, they didn't ban me from the supermarket. Actually, something amazing happened while I was at the till. A young man with a nice camera saw me and filmed me with my salad in the mouth. He was a reporter and wanted to do an interview with me because he had never seen such a good-looking turtoise like me."
+So first the reporter paid my salad and thus calmed the shop assistant's anger and said:
+"I haven't eaten anything today yet! So why don't we sit and eat something together here and I can question you about tortoise-matters? "
+
+
+
+Natürlich verstaut eine Katze ihre Sachen nicht in einer Aktentasche,
+obwohl sich in der Tasche genug Platz finden würde, für alles was eine Katze so zum Leben braucht.
+Vielleicht bewachte die Katze die Aktentasche nur für ihr Herrchen, aber eigentlich wäre das die Aufgabe eines Wachhundes.
+"Ob ich dem Fischer einen Fisch abluchsen könnte? Ich gebe ihm mein bestes und herzzerreißendes Miauen, dann klappt es bestimmt.", sinnierte die Katze.
+Angekommen neben des Fischers Standes auf dem Marktplatz, fing die Katze, die übrigens 
+Melli hieß, ganz süß zu miauen an. Dabei schielte sie zu den Fischen, um ihren Wunsch einer Fischmahlzeit damit auszudrücken.
+<VueExercise lg="de" instruction="Bilde Sätze mit um...zu+Infinitiv, ohne...zu+Infinitiv oder anstatt...zu+Infinitiv wie in den Beispielen!">
+<p>Beispiel 1:</p>
+<p>Er, Hunger, stillen, etwas, essen</p>
+<p>Er isst etwas, um seinen Hunger zu stillen.</p>
+
+<p>Beispiel 2:</p>
+<p>Fernsehen schauen, Hausaufgaben, machen, Er </p>
+<p>Er schaut Fernsehen, anstatt seine Hausaufgaben zu machen.</p>
+
+<p>Beispiel 3:</p>
+<p>Er, gehen, auf Widersehen sagen</p>
+<p>Er geht, ohne auf Widersehen zu sagen.</p>
+
+<ol>
+<li>Gustav, Chef, an jemandem vorbei gehen, jdn grüßen</li>
+<li>Camille, Urlaub machen, arbeiten, lieber</li>
+<li>Katze, putzen, Fell, schmutzig, sein</li>
+</ol>
+<VueHideShow lg="de">
+<ol>
+<li>Gustav geht an seinem Chef vorbei, ohne ihn zu grüßen.</li>
+<li>Camille macht lieber Urlaub, anstatt zu arbeiten.</li>
+<li>Die Katze putzt ihr Fell, um nicht schmutzig zu sein.</li>
+</ol>
+</VueHideShow>
+</VueExercise>
+
+
+"Wenn ich dir einen kleinen Fisch gebe, begann der Fischer, dann möchte ich, dass du ihn woanders verspeist und nicht noch mehr hungrige Mäuler anziehst!" Melli bestätigte die Aussage mit einem Miauen und einem freudigen Wedeln ihres Schwanzes.
+Jedoch ist natürlich eine Aktentasche nicht geeignet, um einen kleinen Fisch darin zu transportieren, sondern in einer Aktentasche transportiert man Akten.
+<VueExercise lg="de" instruction="Fülle die Lücken mit sondern, aber, denn, und oder oder" >
+<VueMCGaps lg="de" :gaptext="['Die Katze heißt nicht Melissa, {sondern} sie heißt Melli.',
+'Die Katze kann zwar nicht bellen, {aber} sie kann miauen.',
+'Die Katze isst einen Fisch, {denn} sie hat Hunger.',
+'Du bezahlst den Fisch {oder} ich gebe ihn dir nicht.',
+'Die Katze mag gerne Fisch {und} Mäuse.']">
+
+</VueMCGaps>
+</VueExercise>
+
+Melli schob die Aktentasche mit ihrem Näschen zum Fischer und sagte ihm so, er solle die kleine Makrele darin verstauen. "Das geht doch nicht!", rief er lachend und setzte nach mit, "Deinem Herrchen würde das sicher nicht gefallen, wenn seine Aktentasche nach Fisch riechen würde!"
+
+<VueExercise lg="de" instruction="Bilde Konditionalsätze wie in den Beispielen!">
+
+<p>Beispiel 1:</p>
+<p>Katze, sehen, Maus, essen (Konditionalsatz der Vergangenheit)</p>
+<p>Wenn die Katze eine Maus gesehen hätte, hätte sie die Maus gegessen.</p>
+
+<ol>
+<li>Katze, Geld, haben, Fisch, bezahlen (Konditionalsatz der Vergangenheit)</li>
+<li>Katze, keinen Hunger, kein Fisch, wollen (Konditionalsatz der Vergangenheit)</li>
+<li>Katze, angeln, können, Fische fangen (Konditionalsatz mit irrealer Bedingung) </li>
+</ol>
+
+<VueHideShow lg="de">
+<ol>
+<li>Wenn die Katze Geld gehabt hätte, hätte sie den Fisch bezahlt.</li>
+<li>Wenn die Katze keinen Hunger gehabt hätte, hätte sie keinen Fisch gewollt.</li>
+<li>Wenn die Katze angeln könnte, würde sie sich selbst Fische fangen.</li>
+</ol>
+</VueHideShow>
+</VueExercise>
+
+
+Bei Fisch in einer Aktentasche war der Handel gestoppt wie bei einer roten Ampel. Beide überlegten, wie die Ampel wieder grün schalten könnten und die Katze machte zuerst einen Vorschlag:
+Die Katze, die ihren kleinen Fisch unter keinen Umständen verlieren wollte, öffnete plötzlich ihr Maul und gab dem sich anbahnenden Gähnen nach.
+"Na also, da haben wir ja schon eine Lösung.", kam es nun vom Fischer, welcher den Fisch der Katze flugs ins Maul steckte.
+Ungünstigerweise wurde die Katze Melli beim Transport des Fisches in ihrem Maul von einer Gruppe von streunenden Katzen beobachtet. Diese streunenden Katzen waren sehr ungebildet und hingen den ganzen Tag lang nur faul herum und fragten sich jetzt, wo Melli den leckeren Fisch herhatte.
+
+<VueExercise lg="de" instruction="Finde das Gegenteil">
+<VueMCGaps lg="de" :gaptext="['öffnen -> {schließen}', 'stoppen -> {starten}', 'klein -> {groß}', 'verlieren -> {gewinnen}', 'faul -> {fleißig}']">
+
+</VueMCGaps>
+
+</VueExercise>
+
+Als Melli darauf nicht wirklich antworten wollte, um dem Fischer keine Probleme zu bereiten, richtete sich die Meute von Katzen gegen sie. Diese nun fauchenden Ungetüme versuchten der armen Melli den Weg zu versperren und sie in eine Ecke der Gasse zu drängen.
+Wie schade! Hätte Melli jetzt das Nudelholz ihrer Großmutter zur Hand, so hätte sie sich damit gut zur Wehr setzten können. Ihre Großmutter hatte öfter Nudeln mit Fisch gekocht.
+In dem kurzen Moment jedoch, wo Melli in alten Erinnerungen schwelgte, war sie den Fisch losgeworden.
+Die Katze, deren Magen knurrte, wollte sich jedoch noch nicht geschlagen geben. Irgendwo musste es doch etwas, für sie zum Fressen geben.
+
+<VueExercise lg="de" instruction="Mache einen Satz aus den zwei Sätzen indem du einen Relativsätz mit Genitiv wie im Beispiel benutzt!">
+<p>Beispiel 1:</p>
+<p>Die Katze lief davon und bezahlte nicht. In ihrem Maul war der Fisch.</p>
+<p>Die Katze, in derem Maul der Fisch war, lief davon und bezahlte nicht.
+
+<ol>
+<li>Das Fell der Katze war dreckig. Die Katze ging unter die Dusche.</li>
+<li>Die Zähne der Katze waren scharf. Die Katze zerriss die Aktentasche aus Versehen.</li>
+<li>Die Stimme des Fischers war sehr kräftig. Der Fischer machte lautstark Werbung für seine Fische.</li>
+</ol>
+
+<VueHideShow lg="de">
+<li>Die Katze, deren Fell dreckig war, ging unter die Dusche.</li>
+<li>Die Katze, deren Zähne scharf waren, zerriss die Aktentasche aus Versehen.</li>
+<li>Der Fischer, dessen Stimme sehr kräftig war, machte lautstark Werbung für seine Fische.</li>
+</VueHideShow>
+</VueExercise>
+
+Keinen Blick in die Schaufenster verschwendend und von ihrem knurrenden Magen getrieben, lief die Katze zurück zum Fischer, wo sie wieder zum Miauen begann.
+Der Fischer war diesmal jedoch nicht allein. An seinem Verkaufsstand, war ein Kunde verkleidet als Osterhase, der seine Bestellungen abholte. Ob der Mann im Kostüm heute Waschtag und deswegen nichts anderes zum Anziehen hatte? Den seltsamen Mann ignorierend maunzte sie sich die Seele aus dem Leib.
+Der Fischer war jetzt völlig verwirrt. Hier der Lärm der miauenden Katze, die doch eben gerade einen Fisch bekommen hatte und jetzt wieder da war, und dort dieser Mann im Osterhasenköstum, obwohl gar kein Ostern war.
+
+<VueExercise lg="de" instruction="Fülle die Lücken mit dem gegebenen Verb im Präteritum aus!">
+<VueMCGaps lg="de" :gaptext="['Die Katze {lief} (laufen) nach Hause.', 'Der Fischer {rief} (rufen) nach der Polizei', 'Die streunenden Katzen {gingen} (gehen) leer aus.', 'Die Katze {hatte} (haben) Hunger.']">
+
+</VueMCGaps>
+</VueExercise>
+
+"Hier ihr bestellter Fisch Hr. Kimmer und ich hoffe sie sind wie immer zufrieden!"
+"Natürlich, ihr Fisch ist schließlich der beste in der Gegend!"
+Beide wünschten sich noch einen schönen Tag und beendeten ihr Gespräch im Eiltempo.
+Der Fischer war eigentlich ein versierter Geschäftsmann. Die Katze hatte zwar nur 50 Cent in ihrer Aktentasche und sein Fisch kostete 1 Euro, dennoch ließ er sich auf den vergünstigten Handel ein, um dem verkaufsschädlichen Katzenjammer an seinem Stand
+ein Ende zu bereiten.
+Überglücklich ob des geglückten Deals, nahm die Katze den gekauften Fisch, doch als sie ihn fressen wollte rief jemand ihren Namen, "Melli! Wo treibst du dich schon wieder rum, meine Mama wird mir den Hosenboden versohlen, wenn wir nicht pünktlich zu Hause sind!" Der kleine Malte nahm die Katze samt Aktentasche auf seine Arme und lief geschwind mit seinem Haustier nach Hause, wo beide hoffentlich keine Standpauke zu hören bekamen.
+
+
+THE END
+
+</div>
+
+
+</template>
+
+<script>
+import VueGaps from "./../../vue-gaps.vue";
+import VueMCGaps from "./../../vue-mc-gaps.vue";
+import VueQuestion from "./../../vue-question.vue";
+import VueExercise from "./../../vue-exercise.vue";
+import VueWholeSentence from "./../../vue-whole-sentence.vue";
+import VueHideShow from "./../../vue-hideshow.vue";
+import VueImage from "./../../vue-image.vue";
+
+export default {
+  
+  components: {
+     VueGaps,
+     VueQuestion,
+     VueExercise,
+     VueWholeSentence,
+     VueHideShow,
+     VueImage,
+     VueMCGaps
+  },
+
+  mounted(){
+     console.log("The mounted function of VueRowFood was called!");
+     
+  },
+  setup(){
+   console.log("The setup function of VueRawFood  is executed!");
+  },
+  data(){
+        return {
+               
+        };
+  },
+  methods: {
+       
+  }
+}
+</script>
+
+<style scoped>
+
+
+
+</style>
