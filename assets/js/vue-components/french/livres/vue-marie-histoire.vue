8c96aecf7a8a7f008ea420d45f8917e9040b6a08
Florian Ingerl <imelflorianingerl@gmail.com>
2024-10-03 07:22:32 +0200

    Finishes the story with Marie

--- assets/js/vue-components/french/livres/vue-marie-histoire.vue
+++ assets/js/vue-components/french/livres/vue-marie-histoire.vue
@@ -1,11 +1,9 @@
 <template>
 <div v-if="$route.params.page == 1">
-<h1>Un livre sur Fred</h1>
-<h4><i>par Florian Ingerl et Marie Faure</i></h4>
+<h1>Une chute douce</h1>
+<h4><i>par Florian Ingerl et <a href="https://mariefaure.fr/">Marie Faure</a></i></h4>
 
-<h1>Chapitre 1</h1>
-<h2>Fred et ses relations</h2>
-
+<img src="assets/img/francais/avion.PNG" style="float: right; height:200px"></img>
 Fred n'était pas paresseux bien que son père l'était. Fred travaillait sans cesse tandis que
 son père demeurait assis dans son fauteuil toute la journée en jouant avec son portable.
 Bien évidemment, son père était bien plus vieux que lui, mais cela n'aurait pas dû l'empêcher d'être plus actif comme l'était sa mère, bien entendu plus vieille que Fred.
@@ -61,6 +59,7 @@ courage, orgueil, générosité, vanité, sérénité, patience, médiocrité, v
 
 </div>
 
+<img src="assets/img/francais/avion.PNG" style="float: right; height:200px"></img>
 "Presque personne ne veut voyager de France en Australie à cette période de l'année.
 C'est pourquoi ils vendent les billets pas cher."
 C'était pour Joséphine une opportunité inattendue et elle n'hésita pas une seconde à la saisir.
@@ -114,7 +113,7 @@ Si un verre {à} vin est {à} un instant donné rempli avec {de} l'eau, on peut 
 </VueMCGaps>
 </VueExercise>
 
-
+<img src="assets/img/francais/avion.PNG" style="float: right; height:200px"></img>
 Evidemment, cet incident n'était pas arrivé par hasard et c'est ce qui interloqua sa mère: " Mais que fais-tu là? Enfin, me voici toute sale et tombée à la renverse! " 
 "Désolé, bredouilla-t-il, tu devrais aller te changer aux toilettes."
 "Tandis que tu seras aux toilettes, je prendrai bien soin de tes affaires et en particulier de tes paquets!"
@@ -130,6 +129,7 @@ Quand on avalait cette poudre dorée, on ne se souvenait plus de tous ses mauvai
 </VueMCGaps>
 </VueExercise>
 
+<p>
 Il se sentit désespéré quand sa mère surgit subitement près de lui. Elle eut pitié de lui en le voyant son paquet à la main, réclamant des explications concernant cette mystérieuse poudre dorée. Allait-elle lui offrir son aide après ce qu'il avait fait?
 Fred était complètement déconcerté et ne trouva aucun mot pour défendre son comportement. Il balbutia simplement: "Dommage que j'aie ouvert ton paquet.... C'était
 par hasard...."
@@ -138,8 +138,101 @@ Sa mère lui répondit aussitôt: " Je ne te permets pas de fouiller dans mes af
 Sa mère se demandait ce qui trottait dans la tête de son fils, pourquoi cette drôle d'attitude depuis qu'ils étaient à l'aéroport. "Je veux partir avec toi en Australie, lui avoua-t-il finalement. "
 "Et pourquoi, s'enquit-elle, surprise"
 "Je m'inquiète pour toi, je ne sais pas ce qui t'attend là-bas."
+<VueExercise lg="fr" :same="true" instruction="Choissis le remplacement pour dire qui convient le mieux!">
+<VueMCGaps lg="fr" :gaptext="['<< Quel age as-tu ? >> {demande|} Fred.',
+'<< J\'ai douze ans >> {réponds|} Camille.',
+'<< Étant donné que j\'ai vingt ans, je suis alors huit ans plus ainé que toi >> {remarque|} Fred.',
+'<< Tu as raison. >> {aquiesce|} Camille.']">
+
+</VueMCGaps>
+</VueExercise>
+
 Heureusement, on pouvait encore acheter un billet pour le vol, bien que ce ne fusse plus 100 euros, mais déjà 120 euros. Alors, le fils et sa mère embarquèrent dans l'avion.
-Malheureusement, leurs places n'étaient pas tout à côté l'une de l'autre mais il y avait seulement un homme assis entre eux. Celui-ci avait l'air d'être un buveur. Ca puait!
+Malheureusement, leurs places n'étaient pas tout à côté l'une de l'autre mais il y avait seulement un homme assis entre eux. Celui-ci avait l'air d'être un buveur. Ca puait! </p>
+
+<VueExercise lg="fr" instruction="Remplis les trous avec la bonne conjugaison!">
+<VueMCGaps lg="fr" :same="true" :gaptext="['{Avant que|} tu puisses embarquer dans un avion, il faut acheter un billet.',
+'{Après|} s\'être assise dans l\'avion, Camille allume le petit télé devant elle.',
+'Mais en regardant le film, elle s\'endorme {parce que|} celui-ci n\'est pas très captivant.']">
+
+</VueMCGaps>
+</VueExercise>
+
+"Qu'est-ce que c'est que cette drôle de poudre dans votre boîte bleue?" demanda l'homme qui sentait si mauvais.
+La mère de Fred lui répondit qu'elle avait trouvé cette poudre dans un endroit absolument insolite qu'il ne pouvait imaginer.
+"Pourrais-je renifler un peu de cette poudre? " voulait savoir l'homme qui puait.
+
+<VueExercise lg="fr" instruction="Remplis les trous avec le bon mot interrogatoire!">
+Quand, combien, comment, pourquoi, Qui, Quoi, Où
+<VueMCGaps lg="fr" :same="true" :gaptext="['{Combien|} de poudre voulez-vous renifler?',
+'{Quand|} avez-vous pris des drogues la dernière fois?',
+'{Pourquoi|} voulez-vous prendre cette poudre? Êtes-vous triste?',
+'{Où} est la cuillère pour sortir un peu de poudre de la boîte?']">
+
+</VueMCGaps>
+</VueExercise>
+
+<img src="assets/img/francais/avion.PNG" style="float: right; height:200px"></img>
+"Cette poudre est fabriquée pour calmer les crocodiles! Elle est déjà utilisée dans les zoos de France, et j'en apporte un peu en Australie pour que les Australiens puissent la tester. Voulez-vous toujours en renifler?" répondit la mère de Fred.
+L'homme, bien qu'étonné par l'origine et l'utilité de cette poudre, ne se sentit aucunement brusqué par la proposition de la mère de Fred, Martine. Il accepta aussitôt.
+Non seulement Fred et sa mère, mais aussi tous les passagers alentours trouvaient cette expérience captivante. Que se passerait-il avec l'homme s'il reniflait le poudre?
+<VueExercise lg="fr" instruction="Formez des phrases conditionelles de type 2!">
+<VueMCGaps lg="fr" :gaptext="['Si la poudre {était} (être) blanche, on {pourrait} (pouvoir) la confondre avec du sucre.',
+'Si l\'homme assis entre Fred et sa mère ne {buvait} (boire) pas, il ne {puerait} (puer) certainement pas.',
+'Si ta cartable {pesait} (peser) moins, tu {aurais} (avoir) moins de maux de dos en la portant.']">
+
+</VueMCGaps>
+</VueExercise>
+
+L'homme se mit très à l'écoute des instructions venant de Martine pour renifler correctement cette poudre mystérieuse.
+Tout le monde cessa de respirer quand l'homme renifla toute la poudre par son nez.
+Après un instant de silence, l'homme donna soudainement une gifle sur la joue du passager derrière lui. Un grand brouhaha commença! L'hôtesse se precipita vers le tumulte pour que cela s'arrête.
+
+<VueExercise lg="fr" instruction="Choisis quelle partie du corps est décrit!">
+<VueMCGaps lg="fr" :same="true" :gaptext="['On peut les utiliser pour compter jusqu\'à dix. => les {doigts|}',
+'On les emploie pour macher de la nourriture. => les {dents|}',
+'On en a deux dans le visage. Quand elles ne fonctionnent plus, il faut porter des lunettes. => les {yeux|}']">
+
+</VueMCGaps>
+</VueExercise>
+
+Et quelle fut sa stupeur lorsqu'elle découvrit un ours assis sur le siège du passager! Quel effroi, quelle farce! D'où pouvait bien venir cette bête sauvage? Etait-ce une hallucination, une réalité? Les autres passagers semblaient eux aussi tellement effrayés qu'elle pensa que ce n'était pas une illusion.
+L'hôtesse eut peur et on la vit disparaître dans le cockpit. Avertirait-elle le pilote qu'il
+y avait un homme devenu fou et un autre passager qui avait illégalement apporté un ours dans l'avion?
+<VueExercise lg="fr" instruction="Trouve le contraire!">
+<VueMCGaps lg="fr" :same="true" :gaptext="['fou => {raissonable|}', 'un homme => {une femme|}', 'bon => {mauvais|}', 'sauvage => {domestique|}', 'réal => {irréal|}', 'la peur => {la bravoure|}', 'légal => {illégal|}', 'disparaître => {apparaître|}']">
+
+</VueMCGaps>
+</VueExercise>
+
+C'est alors qu'une chose étrange se passa. Fred se mit dans tous ses états, ne sachant que penser de cette apparition animale. Et c'est là, dans cet état de stupeur qu'il fit la connaissance d'un homme, un professeur émérite, visiblement un grand scientifique qui lui ne paraissait absolument pas troublé par cet évènement insolite.
+Le prof sortit de son cartable une autre poudre, mais celle-ci n'était pas dorée mais noire.
+<VueExercise lg="fr" instruction="Remplis les trous avec que, qui ou où!">
+<VueMCGaps lg="fr" gaptext="Le prof, {qui} n'avait pas du tout peur, ne renifla pas la poudre noire {qu'} il avait sorti de sa cartable. Il chercha un endroit {où} tout le monde le verrait pour qu'il puisse donner des instructions.">
+
+</VueMCGaps>
+</VueExercise>
+<img src="assets/img/francais/avion.PNG" style="float: right; height:200px"></img>
+"Attention tout le monde!" cria le prof. "J'ai le remède à cette situation. Si vous ne suivez pas mes instructions, notre avion va s'écraser et nous allons tous mourir!"
+C'est alors que le prof se mit à déchirer le sachet de poudre et obligea tout le monde à se taire. En effet, il fallait que le silence soit absolu (malgré le bruit des réacteurs) pour que son remède soit efficace.
+"Il faut que l'ours avale ma poudre noire avec de l'eau en exactement trois gorgées. Que quelqu'un apporte de l'eau maintenant! Et que quelqu'un attrape l'ours!
+
+<VueExercise lg="fr" instruction="Remplis les trous avec la bonne proposition!">
+<VueMCGaps lg="fr" :gaptext="['L\'ours n\'arriva pas {à} avaler la poudre noire en exactement trois gorgées.',
+'En effet, l\'ours vint {d\'} avaler la poudre en une seule gorgée.',
+'Tant pis, c\'est difficile {à} donner des instructions {à} une bête sauvage.']">
+
+</VueMCGaps>
+</VueExercise>
+
+
+En quelques minutes seulement, le tour était joué! Les passagers se ruèrent tout autour de l'ours pour l'immobiliser, tandis que d'autres se chargèrent de verser la poudre dans un grand verre d'eau.
+Mais quel dommage! L'ours avala l'eau en une seule gorgée. L'avion émit un bruit assourdissant et perdit de la hauteur rapidement.
+Mais le pilote glorieux réussira à diriger l'avion dans la Seine où la chute fut presque douce.
+Personne ne mourut.
+
+The End
+
 
 <button @click="nextPageClicked">Next page</button>
 </template>
