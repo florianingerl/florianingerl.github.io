d476cfa0edecb4035b11605a8c7997aee137b731
Florian Ingerl <imelflorianingerl@gmail.com>
2024-10-09 23:15:46 +0200

    Adds a lot of exercises to the story with Max!

---
+++ assets/js/vue-components/french/breveshistoires/vue-marie-deux-oiseaux.vue
@@ -0,0 +1,17 @@
+Antoine était un petit oiseau dont les ailes étaient noires.
+Il était assis sur la branche d'un arbre et savourait le silence qui l'entourait.
+Il adorait étendre ses ailes et dès qu'il se sentait suffisamment à l'aise, il prenait aussitôt son envol. Quelle liberté!
+C'était le printemps. Que faire de cette belle journée ? << Pourquoi ne pas chanter sa meilleure chanson et attirer une jolie femelle avec sa voix si merveilleuse?  >> pensa Antoine.
+Malgré le fait que cette idée de chanter lui plaisait, il ne pouvait s'empêcher de ressentir de la honte car il pensait que ses mélodies n'étaient pas si jolies que celles de son voisin Victor. Alors, il respira longuement pour évacuer ce sentiment fort désagréable et décida de se jeter à l'eau.
+Que ne donnerait-il pas pour qu'une femelle l'aime! Pour l'instant, il essaya de chanter aussi bien qu'il le pouvait. Mais malheureusement, les femelles restèrent à distance et aucune femelle ne s'approcha de lui.
+Cependant, une jolie femelle cachée derrière une branche l'entendit et apprécia sincèrement sa mélodie ; il ne s'agissait aucunement d'avoir pitié de ce jeune oiseau. Pour ne point brusquer ce jeune mâle à l'air anxieux, elle décida de rester cachée pour le moment.
+Après avoir chanté pendant presque une heure, la voix d'Antoine s'affaiblit. Elle avait besoin d'une pause. Il songea donc à se faire un bon repas qui dégagerait une bonne odeur qui, à son tour, attirerait la femelle de ses rêves.
+Puis, il pourrait partager son repas avec elle.
+Il mit immédiatement son plan en œuvre en allant chercher des petits vers et des insectes aux alentours, de sorte qu'il passa (sans la voir) devant la belle femelle.
+La femelle, qui s'appelait Joséphine, sortit de sa cachette parce qu'elle était intriguée par ce que cherchait Antoine. "Que cherches-tu?" demanda-t-elle.
+Antoine faillit répondre << Une jolie femelle telle que toi! >> mais put se maîtriser et dit plûtot : << Je cherche de délicieux vers et insectes. Envie de m'aider? >>
+En dépit de son air timide, Joséphine accepta avec allégresse la proposition d'Antoine.
+Parce qu'il était le mâle, Antoine se sentit obligé de prendre les commandes.
+"Josephine" dit-il. "Veux-tu remuer la terre là-bas avec ton bec? Pendant que tu fais cela, je regarderai par-dessus ton épaule pour voir si tu a trouvé des vers."
+Elle lui sourit aussitôt pour lui montrer qu'elle acquiesçait à sa demande et lui en retour sentit son cœur bondir d'amour.
+Le plan fut mis en œuvre et marcha immédiatement. Ensemble, les deux amoureux mangèrent les délicieux vers, et comme cela ce passe souvent avec des amoureux frais et enthousiastes, ils se marièrent dès le lendemain et eurent beaucoup d'oisillons.
