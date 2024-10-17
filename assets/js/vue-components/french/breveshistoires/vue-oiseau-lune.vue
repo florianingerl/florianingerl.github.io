caf8c7b1f006b1782c47f684cdd540f54c7168be
Florian Ingerl <imelflorianingerl@gmail.com>
2024-10-17 19:01:04 +0200

    Adds story with Edwin!

--- assets/js/vue-components/french/breveshistoires/vue-oiseau-lune.vue
+++ assets/js/vue-components/french/breveshistoires/vue-oiseau-lune.vue
@@ -12,29 +12,32 @@ Linda avait toujours eu envie de posséder un amimal de companie mais ses parent
 Comme sa couleur préférée était l'orange, elle voulait un animal de compagnie ayant une couleur orange.
 
 <VueExercise lg="fr" :topics="['Gérondif']" instruction="Remplis les trous avec le verbe donné au gérondif">
-<VueMCGaps lg="fr" :gaptext="['{Étant}(être) donné que Linda était une humain, elle ne pouvait pas voler comme un oiseau.',
+<VueMCGaps lg="fr" :gaptext="['{Étant}(être) donné que Linda était une humaine, elle ne pouvait pas voler comme un oiseau.',
 '{Ayant}(avoir) toujours peur de l\'obscurité, Linda aime les nuits au pleine lune.']">
 
 </VueMCGaps>
 </VueExercise>
 
-C'était le plus beau oiseau qui ait jamais volé au-dessus de la Terre qu'elle recut enfin pour son dixième anniversaire.
- Et bien sûr, ses ailles étaitent oranges comme les fruits du même nom bien que cet oiseau ne mangeait pas de fruits 
+C'était le plus beau oiseau qui ait jamais volé au-dessus de la Terre qu'elle reçut enfin pour son dixième anniversaire.
+ Et bien sûr, ses ailes étaitent oranges comme les fruits du même nom bien que cet oiseau ne mangeait pas de fruits 
  mais seulement des graines.
-<VueExercise lg="fr" :topics="['Subjonctif']" instrction="Remplis les trous avec le verbe donné au subjonctif!">
-<VueMCGaps lg="fr" :gaptext="['Bien que Linda n\'avait que dix ans, elle {apprenne} (apprendre) très vite comment il faut s\'occuper d\'un oiseau.',
+<VueExercise lg="fr" :topics="['Subjonctif', 'Subjonctif présent']" instrction="Remplis les trous avec le verbe donné au subjonctif!">
+<VueMCGaps lg="fr" :gaptext="['Bien que Linda n\'{aie} (avoir) que dix ans, elle apprend (apprendre) très vite comment il faut s\'occuper d\'un oiseau.',
 'Linda donne des graines à son oiseau pour que celui-ci n\' {aie} (avoir) plus faim.']">
 
 </VueMCGaps>
 </VueExercise>
 
+<img src="assets/img/francais/oiseaudecompanie.png" style="float:right; height: 200px;"></img>
 Elle s'attendait à ne pas devoir s'occuper beaucoup de l´oiseau. Mais l´oiseau avait besoin de beaucoup d'attention. 
 C'est une autre paire de manche à ce qu'elle s'attendait.
 "L'oiseau et moi" pensa Linda. "On pourrait voler ensemble jusqu'à Mars et y rencontrer des extraterrestes. 
 Je ne volerai pas moi-même bien sûr. Mais mon animal me portera."
 
 <VueExercise lg="fr" :topics="['Le Futur simple']" instruction="Remplis les trous avec le verbe donné au futur simple!">
-<VueMCGaps lg="fr" :gaptext="['']">
+<VueMCGaps lg="fr" :gaptext="['Linda et son oiseau {iront} (aller) au Mars.',
+'Ils y {rencontreront} (rencontrer) des extraterrestes.',
+'Tu {liras} (lire) la suite de cette petite histoire.']">
 
 </VueMCGaps>
 </VueExercise>
@@ -44,16 +47,58 @@ Je ne volerai pas moi-même bien sûr. Mais mon animal me portera."
 "Ok allons-y" Dans l´obscurité de la nuit. Il commencèrent leur voyage à travers l´espace.
 "Je suis tellement excité de visiter Mars parce que je n'ai jamais été là-bas" révéla Wilfrid et
 pas besoin de dire, que les deux autres étaient aussi excités que lui.
-"Si nous rencontrons des extraterrestres et s´ils veulent nous manger. Nous devons d'abord réfléchir à une ruse avant de mettre un pied sur mars" disa le singe Wilfried d'un ton inquiet.
-"J'ai une idée!" commenca Linda. "Pour que les extraterrestes n'aient plus envie de nous manger, on va tous se donner la couleur bleu et on va se donner une mauvaise odeur en mangeant de l'ail avant de mettre un pied sur Mars. Comme mesures de précaution.
-"Quand est-ce qu'on arrive? J'ai faim" s'exprima le singe. "Nous sommes bientôt arrivés" réponda Linda. "Avant de manger, il faudra faire connaissance avec les extraterrestres pour ne pas avoir d'ennuis" continua Linda.
-Lorsque Linda vit les extraterrestes et leur apparence physique, elle était très inquiete. Leurs visages étaient très pales et elle soupconna immédiatement un manque de Jod dans leur nourriture.
-"Vous pauvres créatures!" elle leur adressa la parole. "Est-ce que vous voulez manger ces alges que j'ai apporté de la Terre. Cela vous débarassera de votre manque de Jod qui est évident."
-Tu peux m'envoyer un lien pour le Zoom
-"Bonjour et bienvue sur Mars" répondu l'extraterrestre. "Merci beaucoup pour ce cadeau j´en suis très reconnaissant" ajouta-t-il.
+<VueExercise lg="fr" :topics="['Les conjunctions']" instruction="Remplis les trous avec la bonne conjunction!">
+<VueMCGaps lg="fr" :same="true" :gaptext="['La mère de Linda lui achète un oiseau {à condition que|} Linda s\'occupe de lui.',
+'{Parce que|} Wilfried n\'a pas des ailes en tant que singe, il ne peut pas voler.',
+'{Bien que|} le Mars soit loin, Wilfried, Linda et son oiseau y voyagent.']">
+
+</VueMCGaps>
+</VueExercise>
+
+<img src="assets/img/francais/oiseaudecompanie.png" style="float:right; height: 200px;"></img>
+"Si nous rencontrons des extraterrestres et s´ils veulent nous manger. Nous devons d'abord réfléchir à une ruse avant de 
+mettre un pied sur mars" disa le singe Wilfried d'un ton inquiet.
+"J'ai une idée!" commenca Linda. "Pour que les extraterrestes n'aient plus envie de nous manger, on va tous se donner 
+la couleur bleu et on va se donner une mauvaise odeur en mangeant de l'ail
+ avant de mettre un pied sur Mars. Comme mesures de précaution.
+
+<VueExercise lg="fr" :topics="['Les préposition en français']" instruction="Remplis les trous avec la bonne préposition!">
+<VueMCGaps lg="fr" :same="true" :gaptext="['Mars et {entre|} la terre et le Jupiter.',
+'Les planètes tournent {autour du|} soleil.',
+'La terre est {à côte du|} Mars.']">
+
+</VueMCGaps>
+</VueExercise>
+
+"Quand est-ce qu'on arrive? J'ai faim" s'exprima le singe. "Nous sommes bientôt arrivés" réponda Linda. "Avant de manger, 
+il faudra faire connaissance avec les extraterrestres pour ne pas avoir d'ennuis" continua Linda.
+Lorsque Linda vit les extraterrestes et leur apparence physique, elle était très inquiete.
+Leur visage étaient très pales et elle soupconna immédiatement un manque de Jod dans leur nourriture.
+
+<VueExercise lg="fr" :gaptext="['Singulier ou Pluriel en français']" instruction="Choisis entre le singulier et le pluriel!">
+<VueMCGaps lg="fr" :gaptext="['Wilfried, Linda et son oiseau ont emporté {leur brosse à dents|leurs brosses à dents} au Mars.',
+'{Le visage pâle|Les visages pâles} des extraterrestres indiquait une manque de Jod.',
+'{Le cou|Les cous} de Wilfried et Linda n\'était pas aussi long que {celui|ceux} des extraterrestes.']">
+
+</VueMCGaps>
+</VueExercise>
+
+<img src="assets/img/francais/oiseaudecompanie.png" style="float:right; height: 200px;"></img>
+"Vous pauvres créatures!"  leur adressa-t-elle la parole. "Est-ce que vous voulez manger ces alges que j'ai apporté de la Terre.
+ Cela vous débarassera de votre manque de Jod qui est évident."
+
+"Bonjour et bienvue sur Mars" répondu l'extraterrestre. 
+"Merci beaucoup pour ce cadeau, j´en suis très reconnaissant" ajouta-t-il.
 Cela dura un peu avant que les extraterrestes s'apercevaient vraiment qu'ils étaitent guéri
 et qu'ils pouvaient maintenant faire leurs travaux de programmation (ils étaient tous des dévelopeurs de logiciels ) plus efficacement  qu'avant.
 
+<VueExercise lg="fr" :topics="['Des alternatives pour dire']" instruction="Remplis les trous avec le bon remplacement pour dire!" >
+<VueMCGaps lg="fr" :same="true" :gaptext="['<<Je n\'ai pas acheté ces alges mais je les ai volés>> {avoua|} Linda.',
+'<<Pouvez-vous revenir à la terre et en nous apporter d\'autres alges?>> {prièrent|} les extraterrestes.',
+'<<C\'est beaucoup de travail non payé, mais oui, nous pouvons le faire>> {consenta|} Linda.']">
+
+</VueMCGaps>
+</VueExercise>
 
 Fin
 
